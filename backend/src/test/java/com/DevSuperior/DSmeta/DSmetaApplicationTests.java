e580a0e5ce7b5bfc4ba5035be768dff437ffac54
Maria Mariana Cagnoni <mariamariana.cagnoni@gmail.com>
2022-09-07 16:11:04 -0300

    Project created

---
+++ backend/src/test/java/com/DevSuperior/DSmeta/DSmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DevSuperior.DSmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DSmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
